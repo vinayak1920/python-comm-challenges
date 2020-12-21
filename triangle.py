97175a13afd94d3a30a7ca2b4fac55f72d1aa139
vinayak-kamath <vinayak1920@gmail.com>
2020-12-21 16:33:46 +0530

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -2,10 +2,9 @@ s1 = input("Enter side 1: ")
 s2 = input("Enter side 2: ")
 s3 = input("Enter side 3: ")
 
-if s1 == s2:
-  if s2 == s3:
+if s1 == s2 == s3:
     print("Given triangle is equilateral")
-  else:
-    print("Given triangle is isosceles")
-elif s2 != s3:
+elif s2 == s3 or s1 == s2 or s1 == s3:
+  print("Given triangle is isosceles")
+else:
   print("Given triangle is scalene")
